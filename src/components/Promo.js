468525b661351318e5d26cfc91caf2195bfb8276
Artyom <artyom-first@yandex.ru>
2022-11-07 22:46:58 +0500

    feat: добавить лого

--- src/components/Promo.js
+++ src/components/Promo.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import logo from '../images/header__logo.svg';
 
 function Promo() {
   return (
-    <section className="promo">
-
-    </section>
+    <header className="promo">
+      <img className="header__logo" src={logo} alt="лого" />
+    </header>
   );
 }
 
