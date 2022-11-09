0c0f27f794fddac70ddeb560a4dd6eef7eb99d62
Artyom <artyom-first@yandex.ru>
2022-11-09 17:52:19 +0500

    feat: накидать теги для header и promo

--- src/components/Main.js
+++ src/components/Main.js
@@ -4,17 +4,42 @@ import AboutProject from './AboutProject';
 import Techs from './Techs';
 import AboutMe from './AboutMe';
 import Footer from './Footer';
+import logo from '../images/header__logo.svg';
+import planet from '../images/promo__planet.svg';
 
 function Main() {
   return (
-    <main className="main">
-      <Promo />
-      <AboutProject />
-      <Techs />
-      <AboutMe />
-      <Footer />
-    </main>
+    <div className="page">
+      <header className="header">
+        <img className="header__logo" src={logo} alt="лого" />
+        <button className="header__registration-button">Регистрация</button>
+        <button className="header__sign-in-button">Войти</button>
+      </header>
+      <main className="main">
+        <section className="promo">
+          <h1 className="promo__title">Учебный проект студента факультета Веб-разработки.</h1>
+          <p className="promo__subtitle">Листайте ниже, чтобы узнать больше про этот проект и его создателя.</p>
+          <a className="promo__link" href="#about-project">Узнать больше</a>
+          <img className="promo__image" src={planet} alt="веб-мир" />
+        </section>
+        <section className="about-project" id="about-project">
+
+        </section>
+
+
+
+
+      </main>
+    </div>
   );
 }
 
 export default Main;
+
+/*
+        <Promo />
+        <AboutProject />
+        <Techs />
+        <AboutMe />
+        <Footer />
+*/
--- src/components/Promo.js
+++ src/components/Promo.js
@@ -1,10 +1,8 @@
 import React from 'react';
-import logo from '../images/header__logo.svg';
 
 function Promo() {
   return (
     <header className="promo">
-      <img className="header__logo" src={logo} alt="лого" />
     </header>
   );
 }
