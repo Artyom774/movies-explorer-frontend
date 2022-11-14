bb6a34b0951d8b8314fa959b9004be5325f5a624
Artyom <artyom-first@yandex.ru>
2022-11-15 00:40:29 +0500

    refactor: добавить обёртку в about-project

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import Promo from './Promo';
+/* import Promo from './Promo';
 import AboutProject from './AboutProject';
 import Techs from './Techs';
 import AboutMe from './AboutMe';
-import Footer from './Footer';
+import Footer from './Footer'; */
 import logo from '../images/header__logo.svg';
 import planet from '../images/promo__planet.svg';
 
@@ -28,21 +28,25 @@ function Main() {
         </section>
         <section className="about-project" id="about-project">
           <h2 className="about-project__title">О проекте</h2>
-          <div className="about-project__info-container">
-            <p className="about-project__container-caption">Дипломный проект включал 5 этапов</p>
-            <p className="about-project__container-text">Составление плана, работу над бэкендом, вёрстку, добавление функциональности и финальные доработки.</p>
+          <div className="about-project__info-blog">
+            <div className="about-project__info-container">
+              <p className="about-project__container-caption">Дипломный проект включал 5 этапов</p>
+              <p className="about-project__container-text">Составление плана, работу над бэкендом, вёрстку, добавление функциональности и финальные доработки.</p>
+            </div>
+            <div className="about-project__info-container">
+              <p className="about-project__container-caption">На выполнение диплома ушло 5 недель</p>
+              <p className="about-project__container-text">У каждого этапа был мягкий и жёсткий дедлайн, которые нужно было соблюдать, чтобы успешно защититься.</p>
+            </div>
           </div>
-          <div className="about-project__info-container">
-            <p className="about-project__container-caption">На выполнение диплома ушло 5 недель</p>
-            <p className="about-project__container-text">У каждого этапа был мягкий и жёсткий дедлайн, которые нужно было соблюдать, чтобы успешно защититься.</p>
-          </div>
-          <div className="about-project__time-container">
-            <p className="about-project__time-line about-project__time-line_theme_blue">1 неделя</p>
-            <p className="about-project__time-sign">Back-end</p>
-          </div>
-          <div className="about-project__time-container">
-            <p className="about-project__time-line about-project__time-line_theme_blue">4 недели</p>
-            <p className="about-project__time-sign">Front-end</p>
+          <div className="about-project__time-blog">
+            <div className="about-project__time-container">
+              <p className="about-project__time-line about-project__time-line_theme_blue">1 неделя</p>
+              <p className="about-project__time-sign">Back-end</p>
+            </div>
+            <div className="about-project__time-container">
+              <p className="about-project__time-line about-project__time-line_theme_blue">4 недели</p>
+              <p className="about-project__time-sign">Front-end</p>
+            </div>
           </div>
         </section>
       </main>
