240ac0b8a95ef2cf2edbaf240b2e2530a7d24375
Artyom <artyom-first@yandex.ru>
2022-11-17 11:01:14 +0500

    feat: сверстать section about-me for 1280px

--- src/components/Main.js
+++ src/components/Main.js
@@ -6,6 +6,7 @@ import AboutMe from './AboutMe';
 import Footer from './Footer'; */
 import logo from '../images/header__logo.svg';
 import planet from '../images/promo__planet.svg';
+import studentPhoto from '../images/vitalii.jpg';
 
 function Main() {
   return (
@@ -63,7 +64,28 @@ function Main() {
             <p className="techs__tech-name">mpongoDB</p>
           </div>
         </section>
+        <section className="about-me">
+          <h2 className="section-title">Студент</h2>
+          <div className="about-me__info">
+            <h3 className="about-me__name">Артём</h3>
+            <p className="about-me__status">Фронтенд-разработчик, 27 лет</p>
+            <p className="about-me__description">Программированием начал заниматься ещё в магистратуре, так как тема диссертации была связана 
+            с расчётом спектра, что требовало написания отдельной программы. В процессе работы приходилось изучать программирование самостоятельно, 
+            что понравилось, так как это даёт свободу действий и подталкивает к самостоятельному развитию. Выбрал именно веб-разработку ввиду 
+            наглядности результата и возможности хорошего повсеместного представления, достаточно отправить ссылку на сайт.<br />
+Закончил музыкальную школу и до сих пор играю на пианино. Ещё увлекаюсь космонавтикой и астрономией.
+</p>
+            <a className="about-me__link" href="https://github.com/Artyom774">Github</a>
+          </div>
+          <img className="about-me__photo" src={studentPhoto} alt="фото студента" />
+        </section>
+        <section className="portfolio">
+
+        </section>
       </main>
+      <footer className="footer">
+
+      </footer>
     </div>
   );
 }
