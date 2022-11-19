a51c5dcd42971245ad82cb0df9f2515778986a24
Artyom <artyom-first@yandex.ru>
2022-11-19 14:32:16 +0500

    feat: адаптировать footer

--- src/components/Main.js
+++ src/components/Main.js
@@ -99,9 +99,11 @@ function Main() {
       <footer className="footer">
         <p className="footer__text">Учебный проект Яндекс.Практикум x BeatFilm.</p>
         <div className="footer__down-row">
+          <div className="footer__links">
+            <a className="footer__link" href="https://practicum.yandex.ru/web/">Яндекс.Практикум</a>
+            <a className="footer__link" href="https://github.com/Artyom774">Github</a>
+          </div>
           <p className="footer__year">&copy; 2022</p>
-          <a className="footer__link" href="https://practicum.yandex.ru/web/">Яндекс.Практикум</a>
-          <a className="footer__link" href="https://github.com/Artyom774">Github</a>
         </div>
       </footer>
     </div>
