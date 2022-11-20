98e96b213d3bdb4af866c0ca152b3a3377ded9ca
Artyom <artyom-first@yandex.ru>
2022-11-20 12:20:48 +0500

    feat: сверстать нвоый header

--- src/components/MainHeader.js
+++ src/components/MainHeader.js
@@ -6,7 +6,7 @@ function MainHeader() {
     <header className="header header_theme_gray">
       <a href="/"><img className="header__logo" src={logo} alt="лого" /></a>
       <div className="header__links">
-        <a className="header__link" href="/signup">Регистрация</a>
+        <a className="header__link header__link_type_register" href="/signup">Регистрация</a>
         <a className="header__link header__link_type_sign-in" href="/signin">Войти</a>
       </div>
     </header>
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -7,7 +7,14 @@ function Movies() {
     <div className="page">
       <header className="header header_theme_white">
       <a href="/"><img className="header__logo" src={logo} alt="лого" /></a>
-        
+      <div className="header__links">
+        <a className="header__link header__link_type_movies" href="/movies">Фильмы</a>
+        <a className="header__link header__link_type_saved-movies" href="/saved-movies">Сохранённые фильмы</a>
+        <a className="header__links" href="/profile">
+          <p className="header__link header__link_type_profile">Аккаунт</p>
+          <div className="header__profile-link-icon" />
+        </a>
+      </div>
       </header>
       <main className="main">
 
