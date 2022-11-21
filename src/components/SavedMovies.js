617afc193856160c04dc0159e99639f5e69e08a3
Artyom <artyom-first@yandex.ru>
2022-11-22 01:02:39 +0500

    feat: сделать компонент SavedMovies

--- src/components/SavedMovies.js
+++ src/components/SavedMovies.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import Header from './Header';
+import SearchForm from './SearchForm';
+import MoviesCardList from './MoviesCardList';
+import Footer from './Footer';
 
 function SavedMovies() {
   return (
-    <div className="saved-movies">
-      компонент страницы с сохранёнными карточками фильмов
+    <div className="page">
+      <Header />
+      <main className="main">
+        <SearchForm />
+        <MoviesCardList />
+      </main>
+      <Footer />
     </div>
   );
 }
