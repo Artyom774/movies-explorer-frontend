cb49377a9768fb87432ecbacafab781f7824c629
Artyom <artyom-first@yandex.ru>
2022-11-24 17:50:55 +0500

    feat: сверстать адаптированный компонент NotFound

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -3,7 +3,9 @@ import React from 'react';
 function NotFound() {
   return (
     <div className="not-found">
-
+      <h1 className="not-found__title">404</h1>
+      <p className="not-found__text">Страница не найдена</p>
+      <a href="/" className="not-found__link">Назад</a>
     </div>
   );
 }
