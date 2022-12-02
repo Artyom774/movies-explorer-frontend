0ade405a551bb8d4b0ff2df7dcff2aa1e40db001
Artyom <artyom-first@yandex.ru>
2022-12-02 22:50:18 +0500

    feat: настроить валидация для регистрации и авторизации

--- src/components/Login.js
+++ src/components/Login.js
@@ -5,20 +5,58 @@ import { Link } from "react-router-dom";
 function Login({authorizateUser, isSuccess}) {
   const [email, setEmail] = React.useState('');
   const [password, setPassword] = React.useState('');
+  const [emailDirty, setEmailDirty] = React.useState(false);
+  const [passwordDirty, setPasswordDirty] = React.useState(false);
+  const [emailError, setEmailError] = React.useState(false);
+  const [passwordError, setPasswordError] = React.useState(false);
+  const [formValid, setFormValid] = React.useState(false);
+
+  function blurHandler(e) {
+    switch (e.target.name) {
+      case 'email': setEmailDirty(true)
+      break
+      case 'password': setPasswordDirty(true)
+      break
+      default: 
+      break
+    }
+  }
 
   function handleEmail(e) { // отслеживать изменения в поле ввода
     setEmail(e.target.value);
+    const emailRegex = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+    if (!emailRegex.test(String(e.target.value).toLowerCase())) {
+      setEmailError(true);
+    } else {
+      setEmailError(false);
+    };
   }
 
   function handlePassword(e) { // отслеживать изменения в поле ввода
     setPassword(e.target.value);
+    if (e.target.value.length > 0) {
+      setPasswordError(false);
+    } else {
+      setPasswordError(true);
+    };
   }
 
   function handleFormSubmit(e) { // отправить форму
     e.preventDefault();
     authorizateUser(email, password);
-    setPassword('');
   }
+
+  React.useEffect(() => {
+    if (emailError || passwordError) {
+      setFormValid(false);
+    } else {
+      setFormValid(true);
+    };
+  }, [emailError, passwordError])
+
+  React.useEffect(() => {
+    setFormValid(false);
+  }, [])
 
   return (
     <div className="page">
@@ -27,17 +65,32 @@ function Login({authorizateUser, isSuccess}) {
         <h1 className="header__title">Рады видеть!</h1>
       </header>
       <main className="main">
-        <form className="form" onSubmit={handleFormSubmit}>
+        <form className="form" onSubmit={handleFormSubmit} noValidate>
           <div className="form__input-block form__input-block_type_column">
             <p className="form__sign form__sign_type_column">E-mail</p>
-            <input type="text" id="email-input" name="email" value={email} onChange={handleEmail} required className="form__input form__input_type_column" placeholder="E-mail"></input>
+            <input
+              onBlur={blurHandler}
+              type="email" id="email-input" name="email" value={email}
+              onChange={handleEmail}
+              className={`form__input form__input_type_column ` + ((emailDirty && emailError) ? `color_red` : ``)}
+              placeholder="E-mail"></input>
           </div>
           <div className="form__input-block form__input-block_type_column form__input-block_type_no-margin-bottom">
             <p className="form__sign form__sign_type_column">Пароль</p>
-            <input type="password" id="password-input" name="email" value={password} onChange={handlePassword} required className="form__input form__input_type_column" placeholder="Пароль"></input>
+            <input
+              onBlur={blurHandler}
+              type="password" id="password-input" name="password" value={password}
+              onChange={handlePassword}
+              className={`form__input form__input_type_column ` + ((passwordDirty && passwordError) ? `color_red` : ``)}
+              placeholder="Пароль"></input>
           </div>
           <p className={`form__error ` + (isSuccess ? `display_none` : ``)}>Что-то пошло не так...</p>
-          <button type="submit" className="form__submit form__submit_theme_blue">Войти</button>
+          <button
+            disabled={!formValid}
+            type="submit"
+            className={`form__submit form__submit_theme_blue ` + (!formValid ? `form__submit_disabled` : ``)}>
+              Войти
+          </button>
           <div className="form__link-block">
             <p className="form__question">Ещё не зарегистрированы?</p>
             <Link to="/signup" className="form__link">Регистрация</Link>
--- src/components/Register.js
+++ src/components/Register.js
@@ -6,24 +6,72 @@ function Register({registrationUser, isSuccess}) {
   const [name, setName] = React.useState('');
   const [email, setEmail] = React.useState('');
   const [password, setPassword] = React.useState('');
+  const [nameDirty, setNameDirty] = React.useState(false)
+  const [emailDirty, setEmailDirty] = React.useState(false);
+  const [passwordDirty, setPasswordDirty] = React.useState(false);
+  const [nameError, setNameError] = React.useState(false);
+  const [emailError, setEmailError] = React.useState(false);
+  const [passwordError, setPasswordError] = React.useState(false);
+  const [formValid, setFormValid] = React.useState(false);
+
+  function blurHandler(e) {
+    switch (e.target.name) {
+      case 'name': setNameDirty(true)
+      break
+      case 'email': setEmailDirty(true)
+      break
+      case 'password': setPasswordDirty(true)
+      break
+      default: 
+      break
+    }
+  }
 
   function handleName(e) { // отслеживать изменения в поле ввода
     setName(e.target.value);
+    const nameRegex = /^[А-ЯЁA-Z-\s]{2,30}$/umi;
+    if (!nameRegex.test(String(e.target.value))) {
+      setNameError(true);
+    } else {
+      setNameError(false);
+    };
   }
 
   function handleEmail(e) { // отслеживать изменения в поле ввода
     setEmail(e.target.value);
+    const emailRegex = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+    if (!emailRegex.test(String(e.target.value).toLowerCase())) {
+      setEmailError(true);
+    } else {
+      setEmailError(false);
+    };
   }
 
   function handlePassword(e) { // отслеживать изменения в поле ввода
     setPassword(e.target.value);
+    if (e.target.value.length > 0) {
+      setPasswordError(false);
+    } else {
+      setPasswordError(true);
+    };
   }
 
   function handleFormSubmit(e) { // отправить форму
     e.preventDefault();
     registrationUser(email, password, name);
-    setPassword('');
   }
+
+  React.useEffect(() => {
+    if (nameError || emailError || passwordError) {
+      setFormValid(false);
+    } else {
+      setFormValid(true);
+    };
+  }, [nameError, emailError, passwordError])
+
+  React.useEffect(() => {
+    setFormValid(false);
+  }, [])
 
   return (
     <div className="page">
@@ -32,21 +80,41 @@ function Register({registrationUser, isSuccess}) {
         <h1 className="header__title">Добро пожаловать!</h1>
       </header>
       <main className="main">
-        <form className="form" onSubmit={handleFormSubmit}>
+        <form className="form" onSubmit={handleFormSubmit} noValidate>
           <div className="form__input-block form__input-block_type_column">
             <p className="form__sign form__sign_type_column">Имя</p>
-            <input type="text" id="name-input" name="name" value={name} onChange={handleName} required className="form__input form__input_type_column" placeholder="Имя"></input>
+            <input
+              onBlur={blurHandler}
+              type="text" id="name-input" name="name" value={name}
+              onChange={handleName}
+              className={`form__input form__input_type_column ` + ((nameDirty && nameError) ? `color_red` : ``)}
+              placeholder="Имя"></input>
           </div>
           <div className="form__input-block form__input-block_type_column">
             <p className="form__sign form__sign_type_column">E-mail</p>
-            <input type="text" id="email-input" name="email" value={email} onChange={handleEmail} required className="form__input form__input_type_column" placeholder="E-mail"></input>
+            <input
+              onBlur={blurHandler}
+              type="email" id="email-input" name="email" value={email}
+              onChange={handleEmail}
+              className={`form__input form__input_type_column ` + ((emailDirty && emailError) ? `color_red` : ``)}
+              placeholder="E-mail"></input>
           </div>
           <div className="form__input-block form__input-block_type_column form__input-block_type_no-margin-bottom">
             <p className="form__sign form__sign_type_column">Пароль</p>
-            <input type="password" id="password-input" name="email" value={password} onChange={handlePassword} required className="form__input form__input_type_column" placeholder="Пароль"></input>
+            <input
+              onBlur={blurHandler}
+              type="password" id="password-input" name="password" value={password}
+              onChange={handlePassword}
+              className={`form__input form__input_type_column ` + ((passwordDirty && passwordError) ? `color_red` : ``)}
+              placeholder="Пароль"></input>
           </div>
           <p className={`form__error ` + (isSuccess ? `display_none` : ``)}>Что-то пошло не так...</p>
-          <button type="submit" className="form__submit form__submit_theme_blue">Зарегистрироваться</button>
+          <button
+            disabled={!formValid}
+            type="submit"
+            className={`form__submit form__submit_theme_blue ` + (!formValid ? `form__submit_disabled` : ``)}>
+              Зарегистрироваться
+          </button>
           <div className="form__link-block">
             <p className="form__question">Уже зарегистрированы?</p>
             <Link to="/signin" className="form__link">Войти</Link>
