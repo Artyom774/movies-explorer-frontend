c49a2bc46ced263c4f280839d5697022934fdad0
Artyom <artyom-first@yandex.ru>
2022-12-17 13:24:13 +0500

    feat: добавить технологию в section techs

--- src/components/Techs.js
+++ src/components/Techs.js
@@ -4,7 +4,7 @@ const Techs = () => {
   return (
     <section className="techs">
       <h2 className="section-title">Технологии</h2>
-      <h3 className="techs__subtitle">7 технологий</h3>
+      <h3 className="techs__subtitle">8 технологий</h3>
       <p className="techs__text">На курсе веб-разработки мы освоили технологии, которые применили в дипломном проекте.</p>
       <div className="techs__block">
         <p className="techs__tech-name">HTML</p>
@@ -12,6 +12,7 @@ const Techs = () => {
         <p className="techs__tech-name">JS</p>
         <p className="techs__tech-name">React</p>
         <p className="techs__tech-name">Git</p>
+        <p className="techs__tech-name">NodeJS</p>
         <p className="techs__tech-name">Express.js</p>
         <p className="techs__tech-name">mongoDB</p>
       </div>
